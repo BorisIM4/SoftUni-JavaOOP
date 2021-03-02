ab7952a048513a81cc02aafa8d7e71a5220c05f5
JRB <jrb@abv.bg>
2021-03-02 17:39:08 +0200

    Package switch

--- src/Hints.java
+++ src/Hints.java
@@ -4,16 +4,6 @@ public class Hints {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int scoreCounter = 1;
-        double sum = 4;
-
-        for (int i = 2; i <= 10; i++) {
-            sum += 6;
-            scoreCounter++;
-            double average = sum / scoreCounter;
-            System.out.printf("For %d courses, average score is %.2f" ,i , average);
-            System.out.println();
-        }
 
     }
 }
