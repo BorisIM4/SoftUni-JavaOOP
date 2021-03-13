8a2cbff80eedbc6fff36fdc51e45ed8da4487c26
JRB <jrb@abv.bg>
2021-03-13 13:33:36 +0200

    L4-Exer-P2

---
+++ src/L4_Interfaces_and_Abstraction_Exercises/P2_Multiple_Implementation/Identifiable.java
@@ -0,0 +1,5 @@
+package L4_Interfaces_and_Abstraction_Exercises.P2_Multiple_Implementation;
+
+public interface Identifiable {
+    String getId();
+}
---
+++ src/L4_Interfaces_and_Abstraction_Exercises/P2_Multiple_Implementation/Person.java
@@ -0,0 +1,7 @@
+package L4_Interfaces_and_Abstraction_Exercises.P2_Multiple_Implementation;
+
+public interface Person {
+    String getName();
+
+    int getAge();
+}
