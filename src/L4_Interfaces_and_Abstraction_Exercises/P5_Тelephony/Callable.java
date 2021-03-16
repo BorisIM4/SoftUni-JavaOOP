3b25a363240cd6335d1928aec470d1f88cd3081b
JRB <jrb@abv.bg>
2021-03-16 18:18:04 +0200

    L4-Exer-P5

---
+++ src/L4_Interfaces_and_Abstraction_Exercises/P5_Тelephony/Callable.java
@@ -0,0 +1,5 @@
+package L4_Interfaces_and_Abstraction_Exercises.P5_Тelephony;
+
+public interface Callable {
+    String call();
+}
---
+++ src/L4_Interfaces_and_Abstraction_Exercises/P5_Тelephony/Main.java
@@ -0,0 +1,9 @@
+package L4_Interfaces_and_Abstraction_Exercises.P5_Тelephony;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
