6491193f2b617173e78a5ae02fecd5f48c0530e7
JRB <jrb@abv.bg>
2021-03-17 00:24:51 +0200

    L4-Exer-P5

--- src/L4_Interfaces_and_Abstraction_Exercises/P5_Тelephony/Browsable.java
+++
@@ -1,5 +0,0 @@
-package L4_Interfaces_and_Abstraction_Exercises.P5_Тelephony;
-
-public interface Browsable {
-    String browse();
-}
--- src/L4_Interfaces_and_Abstraction_Exercises/P5_Тelephony/Main.java
+++
@@ -1,7 +0,0 @@
-package L4_Interfaces_and_Abstraction_Exercises.P5_Тelephony;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
